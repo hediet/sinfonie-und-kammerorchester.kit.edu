6c99be707936d1de159825eed6bd72cb83e0efac
Henning Dieterichs <henning.dieterichs@hediet.de>
2015-09-11 01:23:39 +0200

    Updated files.

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿$('head').append('<link rel="stylesheet" href="http://sinfonie-und-kammerorchester-kit-edu.netlify.com/style.css" type="text/css" />');
+
+$(document).ready(function() 
+{
+	$(".nav div").each(function(idx, e) { 
+		$(e).html('<a href="' + $("a:contains('" + e.innerText + "')")[0].href + '">' + e.innerText + '</a>'); 
+	});
+
+	$("#pressestimmen-content");
+
+});
