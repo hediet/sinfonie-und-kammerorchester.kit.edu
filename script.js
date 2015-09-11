d0348c7ddba9424aa550d66d1127f7d25da5ed6c
Henning Dieterichs <henning.dieterichs@hediet.de>
2015-09-11 02:11:42 +0200

    Fixed incorrent pressestimmen.

--- script.js
+++ script.js
@@ -3,28 +3,31 @@ var basePath = "//sinfonie-und-kammerorchester-kit-edu.netlify.com/";
 
 $('head').append('<link rel="stylesheet" href="' + basePath + 'style.css" type="text/css" />');
 
-$(document).ready(function() 
-{
-	$(".nav div").each(function(idx, e) { 
-		$(e).html('<a href="' + $("a:contains('" + e.innerText + "')")[0].href + '">' + e.innerText + '</a>'); 
-	});
+$(document).ready(function () {
+    $(".nav div").each(function (idx, e) {
+        $(e).html('<a href="' + $("a:contains('" + e.innerText + "')")[0].href + '">' + e.innerText + '</a>');
+    });
 
-	$("#content .text").each(function(idx, e) {
-		
-		$.getJSON(basePath + "data/pressestimmen.json", function(data) {
-		  var html = "";
-		  $.each(data, function(key, val) {
-			html += "<p>";
-			
-			html += '<a href="' + val.link + '">' + val.title + '</a>';
-			html += val.text;
-			
-			html += "</p>";
-			
-		  });
-		 
-		  $(e).html(html);
-		});
-	});
+    $("#content .text").each(function (idx, e) {
+
+        $.getJSON(basePath + "data/pressestimmen.json", function (data) {
+            var html = "";
+            $.each(data, function (key, val) {
+                html += "<p>";
+
+                var link = val.link;
+
+                link = link.replace(/%resources%/g, basePath + "resources");
+
+                html += '<a href="' + link + '">' + val.title + '</a> ';
+                html += val.text;
+
+                html += "</p>";
+
+            });
+
+            $(e).html(html);
+        });
+    });
 
 });
